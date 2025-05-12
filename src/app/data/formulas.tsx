73e7bb462ccdc307dc6f2f1ba71643e4b2c91831
msyed2020 <mikailsyed@yahoo.com>
2025-05-12 11:01:20 -0400

    first commit

---
+++ src/app/data/formulas.tsx
@@ -0,0 +1,15 @@
+export const formulas = [
+    {
+      id: 'sin',
+      name: 'sin(x)',
+      description: 'Generates a smooth sine wave. Useful for soft tones.',
+      category: 'Basic',
+    },
+    {
+      id: 'sign',
+      name: 'sign(x)',
+      description: 'Returns 1 for positive values and -1 for negative values, creating a square-like wave.',
+      category: 'Basic',
+    },
+    
+  ];
